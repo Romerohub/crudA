1fb400027cb249c2751d2d56ad394f97eb3498c0
Romerohub <diego1415alejoromero@gmail.com>
2023-10-19 10:17:57 -0500

    crud completo con 8 commits

---
+++ js/app.js
@@ -0,0 +1,129 @@
+import { Gift } from "./clases.js";
+
+const cuerpoTabla = document.querySelector("#cuerpo-tabla");
+const myModal = new bootstrap.Modal(document.getElementById("modalGift"));
+const mensajeActualizarDiv = document.querySelector("#mensajeActualizar");
+const mensajeDiv = document.querySelector("#mensaje");
+const mensajeEliminarDiv = document.querySelector("#mensajeEliminar");
+
+let idGiftUpdate = null;
+
+const mostrarMensaje = (mensaje, tipo, div) => {
+div.textContent = mensaje;
+
+const clases = {
+    success: 'alert alert-success',
+    update: 'alert alert-warning',
+    error: 'alert alert-danger'
+};
+
+div.className = clases[tipo];
+div.style.display = 'block';
+
+setTimeout(() => {
+    div.style.display = 'none';
+}, 4000);
+};
+
+const cargarDatosDesdeLocalStorage = () => {
+    const datosGuardados = localStorage.getItem("giftCardsData");
+    if (datosGuardados) {
+        return JSON.parse(datosGuardados);
+    }
+    return [];
+};
+
+const guardarDatosEnLocalStorage = (datos) => {
+    localStorage.setItem("giftCardsData", JSON.stringify(datos));
+};
+
+let datos = cargarDatosDesdeLocalStorage();
+
+const cargarTabla = () => {
+    cuerpoTabla.innerHTML = "";
+    datos.forEach((item) => {
+        const fila = document.createElement("tr");
+        const celdas = `
+            <th>${item.gift}</th>
+            <td>${item.tipo}</td>
+            <td>${item.tiempo}</td>
+            <td>$${item.precio}</td>
+            <td><img src="${item.imagen}" alt="${item.gift}" width="40" height="55"></td>
+            <td>
+                <div class="d-flex gap-2">
+                    <button class="btn btn-outline-warning" onclick="mostrarModal(${item.id})">
+                        <i class="fa fa-pencil" aria-hidden="true"></i>
+                    </button>
+                    <button class="btn btn-outline-danger" onclick="borrarGift(${item.id})">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </button>
+                </div>
+            </td>
+        `;
+        fila.innerHTML = celdas;
+        cuerpoTabla.appendChild(fila);
+    });
+};
+
+const agregarGift = (event) => {
+    event.preventDefault();
+    const id = datos.length > 0 ? datos[datos.length - 1].id + 1 : 1;
+    const gift = document.querySelector("#gift").value;
+    const tipo = document.querySelector("#tipo").value;
+    const tiempo = document.querySelector("#tiempo").value;
+    const precio = document.querySelector("#precio").value;
+    const imagen = document.querySelector("#imagen").value;
+    const nuevaGiftCard = new Gift(id, gift, tipo, tiempo, precio, imagen);
+    datos.push(nuevaGiftCard);
+    guardarDatosEnLocalStorage(datos);
+    document.querySelector("#formGift").reset();
+    cargarTabla();
+    mostrarMensaje(`Producto "${nuevaGiftCard.gift}" Registrado con éxito`, "success", mensajeDiv);
+};
+
+window.mostrarModal = (id) => {
+    idGiftUpdate = id;
+    const index = datos.findIndex((item) => item.id === idGiftUpdate);
+    const modalGift = document.querySelector("#giftModal");
+    const modalTipo = document.querySelector("#tipoModal");
+    const modalTiempo = document.querySelector("#tiempoModal");
+    const modalPrecio = document.querySelector("#precioModal");
+    const modalImagen = document.querySelector("#imagenModal");
+    modalGift.value = datos[index].gift;
+    modalTipo.value = datos[index].tipo;
+    modalTiempo.value = datos[index].tiempo;
+    modalPrecio.value = datos[index].precio;
+    modalImagen.value = datos[index].imagen;
+    myModal.show();
+};
+
+const giftUpdate = (e) => {
+    e.preventDefault();
+    const index = datos.findIndex((item) => item.id === idGiftUpdate);
+    datos[index].gift = document.querySelector("#giftModal").value;
+    datos[index].tipo = document.querySelector("#tipoModal").value;
+    datos[index].tiempo = document.querySelector("#tiempoModal").value;
+    datos[index].precio = document.querySelector("#precioModal").value;
+    datos[index].imagen = document.querySelector("#imagenModal").value;
+    guardarDatosEnLocalStorage(datos);
+    cargarTabla();
+    myModal.hide();
+    mostrarMensaje(`Producto "${datos[index].gift}" Actualizado con éxito`, "update", mensajeActualizarDiv);
+};
+
+window.borrarGift = (id) => {
+const index = datos.findIndex((item) => item.id === id);
+const giftName = datos[index].gift;
+const confirmar = confirm(`¿Está seguro/a de que desea eliminar la Gift Card "${giftName}"?`);
+if (confirmar) {
+    datos.splice(index, 1);
+    guardarDatosEnLocalStorage(datos);
+    cargarTabla();
+    mostrarMensaje(` "${giftName}" Eliminada con éxito`, 'error', mensajeEliminarDiv);
+}
+};
+
+document.querySelector("#formGift").addEventListener("submit", agregarGift);
+document.querySelector("#formModal").addEventListener("submit", giftUpdate);
+
+cargarTabla();
